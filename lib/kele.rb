aff6e3b4f561005bf0ae01ade0e82c557c8520d3
Jeff Wolfram <jeffwolfram@gmail.com>
2017-05-18 16:12:30 -0700

    Made changes to user id attribute

--- lib/kele.rb
+++ lib/kele.rb
@@ -36,7 +36,7 @@ class Kele
 
   end
   def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment)
-    self.class.post(base_url("checkpoint_submissions"), headers: {'authorization' => @auth_token}, body: { 'assignment_branch': assignment_branch, 'assignment_commit_link': assignment_commit_link, 'checkpoint_id': checkpoint_id, 'comment': comment, 'enrollment_id': enrollment_id } )
+    self.class.post(base_url("checkpoint_submissions"), headers: {'authorization' => @auth_token}, body: { 'assignment_branch': assignment_branch, 'assignment_commit_link': assignment_commit_link, 'checkpoint_id': checkpoint_id, 'comment': comment, 'enrollment_id': get_me["current_enrollment"]["id"] } )
 
   end
   private
